beb0fb4cbebcb27163e0223c8f087526cd4db51f
Kevin Washington <kevw12188@gmail.com>
2015-11-11 21:35:50 -0500

    refactord verifyToken.js with httpResponder methods

--- app/middleware/verifyToken.js
+++ app/middleware/verifyToken.js
@@ -3,37 +3,26 @@ var httpResponses = require("constants/httpResponses");
 var tokenSecret = require("private/appSecrets").tokenSecret;
 var jwt = require("jsonwebtoken");
 var RevokedToken = require("resources/revokedTokens/model");
+var httpResponder = require("shared/httpResponder");
 
 router.use(function(req, res, next) {
     var token = req.body.token || req.query.token || req.headers["x-access-token"];
     if(!token){
-        return res.json({
-            status: httpResponses.failureResponseStatus,
-            statusCode: httpResponses.failureUnauthorizedStatusCode,
-            data: {
-                message: httpResponses.failureNoTokenProvidedMessage
-            }
-        });
+        return res.json(
+            httpResponder.respondToUnauthorizedRequest(httpResponses.failureNoTokenProvidedMessage)
+        );
     }
     RevokedToken.findOne({token: token}, function(err, revokedToken) {
         if(revokedToken) {
-            return res.json({
-                status: httpResponses.failureResponseStatus,
-                statusCode: httpResponses.failureUnauthorizedStatusCode,
-                data: {
-                    message: httpResponses.failureInvalidTokenMessage
-                }
-            });
+            return res.json(
+                httpResponder.respondToUnauthorizedRequest(httpResponses.failureInvalidTokenMessage)
+            );
         }
         jwt.verify(token, tokenSecret, function(err, decoded) {
             if(!decoded) {
-                return res.json({
-                    status: httpResponses.failureResponseStatus,
-                    statusCode: httpResponses.failureUnauthorizedStatusCode,
-                    data: {
-                        message: httpResponses.failureInvalidTokenMessage
-                    }
-                })
+                return res.json(
+                    httpResponder.respondToUnauthorizedRequest(httpResponses.failureInvalidTokenMessage)
+                );
             }
             req.decoded = decoded;
             next();
