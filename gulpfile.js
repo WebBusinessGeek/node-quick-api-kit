8b8b0afa7c799b3880a239317cda4ff1b23e756a
Kevin Washington <kevw12188@gmail.com>
2015-11-12 16:49:06 -0500

    gulpfile now writes quotations correctly

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require("gulp");
 var shell = require("gulp-shell");
 
 gulp.task("setup", shell.task([
-    "npm install",
     "node_modules/.bin/epr",
     "touch app/private/databaseSecrets.js app/private/appSecrets.js",
-    "echo exports.databaseUri = 'uri goes here' > app/private/databaseSecrets.js",
-    "echo exports.tokenSecret = 'token secret goes here' > app/private/appSecrets.js",
-    "echo project setup"
+    "echo exports.databaseUri = '\"mongoose db uri goes here\";' > app/private/databaseSecrets.js",
+    "echo exports.tokenSecret = '\"secret for jwt goes here\";' > app/private/appSecrets.js",
+    "echo Project has been setup. Add private secrets and then run '\"npm test\"'"
 ]));
