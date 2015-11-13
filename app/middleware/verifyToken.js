99b1bafe9ea4f36d48c3f865b19a89669bb170ee
Kevin Washington <kevw12188@gmail.com>
2015-11-12 20:25:01 -0500

    removed all strings from verifyToken.js

--- app/middleware/verifyToken.js
+++ app/middleware/verifyToken.js
@@ -4,9 +4,10 @@ var tokenSecret = require("private/appSecrets").tokenSecret;
 var jwt = require("jsonwebtoken");
 var RevokedToken = require("resources/revokedTokens/model");
 var httpResponder = require("shared/httpResponder");
+var serverStatics = require("constants/serverStatics");
 
 router.use(function(req, res, next) {
-    var token = req.body.token || req.query.token || req.headers["x-access-token"];
+    var token = req.body.token || req.query.token || req.headers[serverStatics.headerAccessTokenKey];
     if(!token){
         return res.json(
             httpResponder.respondToUnauthorizedRequest(httpResponses.failureNoTokenProvidedMessage)
