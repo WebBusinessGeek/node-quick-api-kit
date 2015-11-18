e77caaacd8fc5654aec25baebc95ad29f0622425
Kevin Washington <kevw12188@gmail.com>
2015-11-18 11:40:57 -0500

    added cross origin requests

--- app/bootstrap/app.js
+++ app/bootstrap/app.js
@@ -14,6 +14,11 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //user Bootstrap Routes
 var userEndpoint = exports.usersEndpoint = endpoints.usersEndpoint;
